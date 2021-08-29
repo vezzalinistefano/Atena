364e0acf370388563730805be220f763d7ebb5fa
Stefano Vezzalini <ste.vezzalini@gmail.com>
2021-08-29 14:06:54 +0200

    updated access control to update and delete views

--- shop/mixins.py
+++ shop/mixins.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 class OwnershipMixin(LoginRequiredMixin, UserPassesTestMixin):
 
     def test_func(self):
-        return self.request.user.id == self.kwargs['pk']
+        print(self.kwargs)
+        return self.request.user.id == self.kwargs['teacher_id']
 
     def handle_no_permission(self):
         return render(request=self.request, template_name='permission_denied.html')
--- shop/urls.py
+++ shop/urls.py
@@ -8,8 +8,8 @@ app_name = 'shop'
 
 urlpatterns = [
     path('course/create', CourseCreate.as_view(), name='course-create'),
-    path('course/<int:pk>/update', CourseUpdate.as_view(), name='course-update'),
-    path('course/<int:pk>/delete', CourseDelete.as_view(), name='course-delete'),
+    path('course/<int:pk>/<int:teacher_id>/update', CourseUpdate.as_view(), name='course-update'),
+    path('course/<int:pk>/<int:teacher_id>/delete', CourseDelete.as_view(), name='course-delete'),
     path('course/<int:pk>/detail', CourseDetail.as_view(), name='course-detail'),
     path('course/list', CourseList.as_view(), name='course-list'),
     path('course/<int:pk>/purchase', CoursePurchase.as_view(), name='course-purchase')
